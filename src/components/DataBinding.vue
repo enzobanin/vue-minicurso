5dc4db480805ab87bc1c1b99d43da473ed7e3739
enzobanin <e.banin@aluno.ifsp.edu.br>
2025-09-29 16:49:17 -0300

    DataBinding

---
+++ src/components/DataBinding.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="componente">
+        <h1>Componente com Data Binding</h1>
+        <p>{{ valor }}</p>
+        <!-- ONE WAY DATA BINDING <input type="text" v-bind:value="valor"></input> -->
+        <!-- TWO WAY DATA BINDING<input type="text" v-bind:value="valor" v-on:input="(event)=> valor = event.target.value"></input> -->
+        <!-- COM ATALHOS <input type="text" :value="valor" @input="(event)=> valor = event.target.value"></input>-->
+        <input type="text" v-model="valor"></input>
+    </div>
+</template>
+
+<script setup>
+    import {ref} from "vue";
+    const valor = ref("Vue é um bom framework");
+</script>
