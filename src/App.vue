fdab1e63b551d410f34f0c722740b6f209b72e68
enzobanin <e.banin@aluno.ifsp.edu.br>
2025-10-02 16:46:30 -0300

    AdicionandoEventos

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,17 @@
   import ComDadosReativosPrimitivos from './components/ComDadosReativosPrimitivos.vue';
   import ComDadosReativosNaoPrimitivos from './components/ComDadosReativosNaoPrimitivos.vue';
   import DataBinding from './components/DataBinding.vue';
+  import ComProps from './components/ComProps.vue';
+  import EscutaEvento from './components/EscutaEvento.vue';
+
 </script>
 
 <template>
   <header>Vue</header>
+  <EscutaEvento/>
+  <ComProps item = "Borracha" :preco="2.50"/> <!-- usa-se o v-bind(:) para o preco não ser uma string>-->
+  <!-- <ComProps item = "Lápis" :preco="1.25"/>
+  <ComProps item = "Apontador" :preco="3.95"/> -->
   <DataBinding/>
   <ComDadosReativosNaoPrimitivos/>
   <ComDadosReativosPrimitivos/>
---
+++ src/components/ComProps.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="componente">
+        <h1>Componente com Props</h1>
+        <p>{{ item }}(R${{ preco.toFixed(2) }})</p>
+    </div>
+</template>
+
+<script setup>
+    import { defineProps } from 'vue';
+
+    defineProps({ //define as propriedades que o componente vai receber
+        item: String,
+        preco: Number
+    })
+
+    // const item = "ooo" , mesma forma de usar do defineProps
+</script>
